1f7ae969a57c35aacd64cdc5148af00086ca8e56
asltec <adautosl.lucas@gmail.com>
2020-10-13 07:20:36 -0300

    Correcao app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const cors = require('cors');
-const bodyParser = require('body-parser');
+
 
 app.use(cors()); 
 app.use(express.urlencoded({ extended: false }));
